e08652a829015d60111ef996f7e887f950bd73be
Jack <Jwenschhof@bsd220.org>
2022-05-12 11:00:30 -0500

    Added Flag quiz

---
+++ Guess the Country/QuizView.swift
@@ -0,0 +1,42 @@
+//
+//  QuizView.swift
+//  Guess the Country
+//
+//  Created by Student on 5/12/22.
+//
+
+import SwiftUI
+ 
+struct QuizView: View {
+    @EnvironmentObject var quizManager: QuizManager
+    var body: some View {
+        if quizManager.reachedEnd {
+            VStack(spacing: 20) {
+                Text("Country Flag Quiz")
+                    .font(.title)
+                Text("Congratulations! you have completed the quiz.")
+                Text("You scored \(quizManager.score) out of \(quizManager.length)")
+                Button {
+                    quizManager.reset()
+                } label: {
+                    primaryButton(text: "play again")
+                }
+            }
+            .foregroundColor(Color("AccentColor"))
+            .padding()
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color(hue: 0.599, saturation: 0.433, brightness: 0.971))
+        }
+        else {
+            QuestionView()
+                .environmentObject(quizManager)
+        }
+    }
+}
+ 
+struct QuizView_Previews: PreviewProvider {
+    static var previews: some View {
+        QuizView()
+            .environmentObject(QuizManager())
+    }
+}
