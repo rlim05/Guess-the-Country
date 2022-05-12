7db776fd0bda2c6007758e128191575bb25a3663
Ryan Lim <rlim@bsd220.org>
2022-05-12 10:19:21 -0500

    Initial Commit

---
+++ Guess the Country/Guess_the_CountryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_the_CountryApp.swift
+//  Guess the Country
+//
+//  Created by Student on 5/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Guess_the_CountryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
